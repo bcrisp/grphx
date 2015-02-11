c2eb53856f40727ed800f20d2c0f5a13e543e84c
Brandon Crisp <brandoncrisp@Brandons-MacBook-Pro-2.local>
2015-02-10 21:31:15 -0800

    Initial commit

---
+++ app/controllers/Entries.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import org.apache.spark.SparkContext._
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+/**
+ * Created by brandoncrisp on 1/24/15.
+ */
+object Entries extends Controller{
+  def submit = Action {
+
+    val conf = new SparkConf()
+      .setAppName("Load graph")
+      .setMaster("local[2]")
+      .set("spark.driver.allowMultipleContexts", "true")
+      .setJars(SparkContext.jarOfClass(this.getClass).toList)
+
+    val sc = new SparkContext(conf)
+
+    // Create an RDD for the vertices
+    val users: RDD[(VertexId, (String, String))] =
+      sc.parallelize(Array((3L, ("rxin", "student")), (7L, ("jgonzal", "postdoc")),
+        (5L, ("franklin", "prof")), (2L, ("istoica", "prof"))))
+    // Create an RDD for edges
+    val relationships: RDD[Edge[String]] =
+      sc.parallelize(Array(Edge(3L, 7L, "collab"),    Edge(5L, 3L, "advisor"),
+        Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
+    // Define a default user in case there are relationship with missing user
+    val defaultUser = ("John Doe", "Missing")
+    // Build the initial Graph
+    val graph = Graph(users, relationships, defaultUser)
+
+    // Count all users which are postdocs
+    val vertices = graph.vertices.filter { case (id, (name, pos)) => pos == "prof" }.count
+    // Count all the edges where src > dst
+    val edges = graph.edges.filter(e => e.srcId > e.dstId).count
+
+    Ok(views.html.index("Your new application is now ready Grphax." + vertices))
+  }
+
+}
